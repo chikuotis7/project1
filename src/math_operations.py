ddf93b3091ee2e05e8edcd501a76e693132f38df
chikuotis7 <chikuotis5@gmail.com>
2026-04-09 11:57:24 +0530

    9 file

--- src/math_operations.py
+++ src/math_operations.py
@@ -2,20 +2,16 @@ def add(a: float, b: float):
     return a + b
 
 
-
 def sub(a: float, b: float):
     return a - b
-
 
 
 def mul(a: float, b: float):
     return a * b
 
 
-
 def div(a: float, b: float):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
-
 
